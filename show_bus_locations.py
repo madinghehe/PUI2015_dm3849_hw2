24f8484fb03d7a69d177cc2d677c407d34aba2e1
madinghehe <dm3849@nyu.edu>
2015-09-24 14:55:38 -0400

    assignmen1

--- show_bus_locations.py
+++ show_bus_locations.py
@@ -3,14 +3,16 @@ import sys
 import csv
 import urllib2
 if __name__=='__main__':
-    #key = '37e65b06-ad6d-402c-977a-22345d1c2ec9'
-    #bus = 'B52'
+    key = '37e65b06-ad6d-402c-977a-22345d1c2ec9'
+    bus = 'B52'
     url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s' % (sys.argv[1],sys.argv[2])
     request = urllib2.urlopen(url)
     metadata = json.loads(request.read())
     count = 0
-    
-    for i in metadata['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']:
+    item = metadata['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+    print 'Bus Line = %s' % sys.argv[2]
+    print 'Number of Active = %d' % len(item)
+    for i in item:
         
         print 'Bus %d is at Latitude %s and Longitude %s' % (count, i['MonitoredVehicleJourney']['VehicleLocation']['Latitude'], i['MonitoredVehicleJourney']['VehicleLocation']['Longitude'])
         count += 1
